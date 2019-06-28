4042be7e7c116a1937752ae6f94b4001b3273120
hirata-dat <49702207+hirata-dat@users.noreply.github.com>
2019-06-28 15:45:22 +0900

    タイトルからゲームフロー

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+bool sceneChanged = false;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sceneChanged == true) return;
+        if(Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Game");
+        }
+    }
+}
